209db861b1b2b905ed04ab88c9a28c8d3ffc1a65
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2018-06-04 11:09:50 -0700

    Add EF projects and models using Northwind DB

---
+++ EFCoreReview.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using EFCoreReview.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EFCoreReview.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestEFCore();
+            Console.ReadLine();
+        }
+
+        static void TestEFCore()
+        {
+            using (var context = new NorthwindContext())
+            {
+                var categories = context.Categories.ToList();
+
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"Category {category.CategoryName} - {category.Description}");
+                }
+            }
+        }
+    }
+}
