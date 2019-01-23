a239558769657cd670282500fc21b4a42a357856
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-01-23 15:47:34 -0800

    Add project for reviewing migrations
    This project targets the .net standard and .net core 2.1 frameworks.

---
+++ MigrationReview/Model/MyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace MigrationReview.Model
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(ConfigurationManager.ConnectionStrings["MigrationDb"].ConnectionString);
+            }
+        }
+    }
+}
---
+++ MigrationReview/Model/Supplier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MigrationReview.Model
+{
+    public class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
