bd50c41d1f28b6a71befe14b204c9ccaaf2a80ec
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-01-23 16:07:46 -0800

    Clean up program.cs

---
+++ EFCoreReview.App/OverrideContext.cs
@@ -0,0 +1,37 @@
+﻿using EFCoreReview.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EFCoreReview.App
+{
+    public class OverrideContext
+    {
+        /// <summary>
+        /// This is to show how you can override the options and use in memory database instead
+        /// </summary>
+        static void TestOverride()
+        {
+            using (var context = GetContext())
+            {
+                context.Categories.Add(new Categories { CategoryName = "new", Description = "test" });
+
+                context.SaveChanges();
+
+                var categories = context.Categories.ToList();
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"Category {category.CategoryId}: {category.CategoryName} - {category.Description}");
+                }
+            }
+        }
+
+        static NorthwindContext GetContext()
+        {
+            var dbOptions = new DbContextOptionsBuilder<NorthwindContext>()
+                .UseInMemoryDatabase("Test").Options;
+
+            return new NorthwindContext(dbOptions);
+        }
+    }
+}
--- EFCoreReview.App/Program.cs
+++ EFCoreReview.App/Program.cs
@@ -1,51 +1,10 @@
-﻿using EFCoreReview.Data.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace EFCoreReview.App
+﻿namespace EFCoreReview.App
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //TestOverride();
-            //AggregateFunctions.ReviewSum();
-            //RandomQueries.ReturnOnlyOneColumn();
-            //RandomQueries.TestQuery();
-            //RandomQueries.PassingNullWhereValue();
-
-            new DataQueryExamples().GetEmployeeWhereAndOrderBy().Wait();
 
         }
-
-        /// <summary>
-        /// This is to show how you can override the options and use in memory database instead
-        /// </summary>
-        static void TestOverride()
-        {
-            using (var context = GetContext())
-            {
-                context.Categories.Add(new Categories { CategoryName = "new", Description = "test" });
-
-                context.SaveChanges();
-
-                var categories = context.Categories.ToList();
-                foreach (var category in categories)
-                {
-                    Console.WriteLine($"Category {category.CategoryId}: {category.CategoryName} - {category.Description}");
-                }
-            }
-        }
-
-        static NorthwindContext GetContext()
-        {
-            var dbOptions = new DbContextOptionsBuilder<NorthwindContext>()
-                .UseInMemoryDatabase("Test").Options;
-
-            return new NorthwindContext(dbOptions);
-        }
-
     }
 }
