5c606398fece657e50cc9aa38e4fb900a32a1d29
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-01-23 16:04:21 -0800

    Add a class for running migration at runtime

---
+++ EFCoreReview.App/Migration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationReview.Model;
+
+namespace EFCoreReview.App
+{
+    public class Migration
+    {
+        public void RunMigration()
+        {
+            using (var context = new MyDbContext())
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
