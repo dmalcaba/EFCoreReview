0604c2f9c3f79ad6ff80f62ba9dceb5ea9856e88
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-01-25 11:18:41 -0800

    Add Migration2Review project
    This is for reviewing the use of OnModelCreating

---
+++ Migration2Review/Model/UserDbContext.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Migration2Review.Model
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext()
+        {
+        }
+
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(ConfigurationManager.ConnectionStrings["MigrationDb"].ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity => {
+                entity.HasKey(e => e.UserId);
+
+                entity.Property(e => e.UserId)
+                    .HasColumnName("UserId")
+                    .ValueGeneratedOnAdd()
+                    .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+                entity.Property(e => e.UserName)
+                    .IsRequired()
+                    .HasColumnName("UserName")
+                    .HasMaxLength(100);
+            });
+        }
+    }
+}
