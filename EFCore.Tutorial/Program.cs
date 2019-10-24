3329dd1817a3ac081546c291737bf530652a2d6e
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2019-10-24 16:04:03 -0700

    Add new project for tutorial

---
+++ EFCore.Tutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EFCore.Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            new Queries.Tutorial().GetAllEmployees().Wait();
+
+        }
+    }
+}
---
+++ EFCore.Tutorial/Queries/Tutorial.cs
@@ -0,0 +1,28 @@
+﻿using EFCoreReview.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Tutorial.Queries
+{
+    public class Tutorial
+    {
+        private readonly NorthwindContext _context;
+
+        public Tutorial()
+        {
+            _context = new NorthwindContext();
+        }
+
+        public async Task GetAllEmployees()
+        {
+            var query = _context.Employees;
+
+            var result = await query.ToListAsync();
+
+            Console.WriteLine("\nTotal Employees {0}", result.Count);
+        }
+    }
+}
