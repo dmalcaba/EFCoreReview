c62f9f2c15d50aa42209cefb4e84428e904cd544
Darryl Malcaba <34329210+dmalcaba@users.noreply.github.com>
2020-01-17 16:46:19 -0800

    Add Employee Dtos

---
+++ EFCore.Tutorial/Dtos/EmployeeDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.Tutorial.Dtos
+{
+    public class EmployeeDto
+    {
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public string BirthdateString { get; set; }
+        public string Address { get; set; }
+        public string FullName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ EFCore.Tutorial/Dtos/EmployeeGridDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore.Tutorial.Dtos
+{
+    public class EmployeeGridDto
+    {
+        public int EmployeeId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string BirthdateString { get; set; }
+        public string City { get; set; }
+    }
+}
